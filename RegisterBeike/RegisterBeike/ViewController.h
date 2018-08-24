2027dda08708a1b7cca3497e6c751d20e9c63b64
g623171541 <wwwhappygxd@163.com>
2018-08-24 18:38:24 +0800

    注册贝壳APP项目

---
+++ RegisterBeike/RegisterBeike/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  RegisterBeike
+//
+//  Created by paddygu on 2018/8/24.
+//  Copyright © 2018年 paddygu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ RegisterBeike/RegisterBeike/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  RegisterBeike
+//
+//  Created by paddygu on 2018/8/24.
+//  Copyright © 2018年 paddygu. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+@property (weak, nonatomic) IBOutlet UILabel *mobileLabel;
+@property (weak, nonatomic) IBOutlet UIImageView *codeImageView;
+@property (weak, nonatomic) IBOutlet UITextField *imageCodeField;
+@property (weak, nonatomic) IBOutlet UILabel *msgCodeLabel;
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
