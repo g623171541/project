bb506d82409ab64c33c0e314e6794069f1291544
谷幸东 <Paddygu@feelwx.com>
2018-08-25 23:58:24 +0800

    布局及引用第三方

--- RegisterBeike/RegisterBeike/ViewController.h
+++ RegisterBeike/RegisterBeike/ViewController.h
@@ -7,6 +7,8 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <AVFoundation/AVFoundation.h>
+//#import <UIView+Toast.h>
 
 @interface ViewController : UIViewController
 
--- RegisterBeike/RegisterBeike/ViewController.m
+++ RegisterBeike/RegisterBeike/ViewController.m
@@ -13,6 +13,8 @@
 @property (weak, nonatomic) IBOutlet UIImageView *codeImageView;
 @property (weak, nonatomic) IBOutlet UITextField *imageCodeField;
 @property (weak, nonatomic) IBOutlet UILabel *msgCodeLabel;
+@property (weak, nonatomic) IBOutlet UILabel *loginMsgCodeField;
+@property (weak, nonatomic) IBOutlet UILabel *editMsgCodeField;
 
 @end
 
@@ -20,14 +22,53 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
+    
+
+}
+// 获取手机号码
+- (IBAction)getMobile:(UIButton *)sender {
+    
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+// 获取图片验证码
+- (IBAction)getImageWithCode:(UIButton *)sender {
 }
 
+// 发送短信验证码
+- (IBAction)sendMsgCode:(UIButton *)sender {
+}
+
+// 获取短信验证码
+- (IBAction)getMsgCode:(UIButton *)sender {
+}
+
+// 注册
+- (IBAction)registerBeike:(UIButton *)sender {
+}
+
+// 发送登录验证码
+- (IBAction)sendLoginMsgCode:(UIButton *)sender {
+}
+
+// 获取登录验证码
+- (IBAction)getLoginMsgCode:(UIButton *)sender {
+}
+
+// 验证码登录
+- (IBAction)loginWithMsgCode:(UIButton *)sender {
+}
+
+// 修改密码-获取登录验证码
+- (IBAction)getMsgCodeForEditPwd:(UIButton *)sender {
+}
+
+// 修改密码
+- (IBAction)editPwd:(UIButton *)sender {
+}
+
+// 账号密码登录
+- (IBAction)loginWithPwd:(UIButton *)sender {
+}
 
 @end
