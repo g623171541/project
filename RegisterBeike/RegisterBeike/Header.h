a0fdd25a0b00246ca19116a7d2932bf6850d00f9
谷幸东 <Paddygu@feelwx.com>
2018-09-10 23:36:13 +0800

    提交结束该项目
    没有成功，后期有时间再搞

--- RegisterBeike/RegisterBeike/Header.h
+++ RegisterBeike/RegisterBeike/Header.h
@@ -8,7 +8,9 @@
 
 #ifndef Header_h
 #define Header_h
+// 易码平台购买了 短信接收服务 http://www.51ym.me/User/apidocs.html#getsms
 
 #define TOKEN              @"008008420e5f905c2c1dc7b07816d04af57ce8ed"
+
 
 #endif /* Header_h */
